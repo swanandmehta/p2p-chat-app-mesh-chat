84b393fe856dc8db1bbfdcce8cc7d442c41f1e51
swanandm <swanandmehta94@gmail.com>
2025-02-13 01:21:12 +0100

    Created modules and basic routes

--- mesh-chat/src/app/app.component.ts
+++ mesh-chat/src/app/app.component.ts
@@ -7,6 +7,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
---
+++ mesh-chat/src/app/communication/communication.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CommunicationRoutingModule } from './communication-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CommunicationRoutingModule
+  ]
+})
+export class CommunicationModule { }
